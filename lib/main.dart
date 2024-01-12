fbfe2de0d479adeea8f8be4ae960d4cd95dc8ba4
vinhphuoc1892 <phuocnv1@hoaphat.com.vn>
2024-01-12 15:33:26 +0700

    hpdq3

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter HPDQ2',
+      title: 'Flutter HPDQ23',
       theme: ThemeData(
         // This is the theme of your application.
         //
